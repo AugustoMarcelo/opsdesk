fb4339134f153fc2319b8bad77709bddb1fd6cbb
Marcelo <marcelo.augusto@99x.io>
2025-12-26 13:20:19 -0300

    chore: bootstrap monorepo, api, docker infra and observability

---
+++ apps/api/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { requestIdMiddleware } from './common/middleware/request-id.middleware';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.use(requestIdMiddleware);
+  app.useLogger(app.get(Logger));
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
