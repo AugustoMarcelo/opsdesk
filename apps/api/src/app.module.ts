e71ab37563bae77cc0d37247b3fc56ae85c7a30c
Marcelo Augusto <mrclgst10@gmail.com>
2025-12-26 13:20:19 -0300

    chore: bootstrap monorepo, api, docker infra and observability

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { HealthModule } from './health/health.module';
+import { LoggerModule } from './logger/logger.module';
+
+@Module({
+  imports: [LoggerModule, HealthModule],
+})
+export class AppModule {}
