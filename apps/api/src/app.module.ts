39c5b8efe6e6840fd968ecb059eb477324cbda15
Marcelo Augusto <mrclgst10@gmail.com>
2025-12-29 16:46:26 -0300

    State and commit

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,16 @@ import { LoggerModule } from './logger/logger.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { MetricsModule } from './metrics/metrics.module';
 import { MetricsInterceptor } from './metrics/metrics.interceptor';
+import { RabbitMQModule } from './messaging/rabbitmq.module';
 
 @Module({
-  imports: [LoggerModule, HealthModule, TicketsModule, MetricsModule],
+  imports: [
+    LoggerModule,
+    HealthModule,
+    TicketsModule,
+    MetricsModule,
+    RabbitMQModule,
+  ],
   providers: [
     {
       provide: 'APP_INTERCEPTOR',
