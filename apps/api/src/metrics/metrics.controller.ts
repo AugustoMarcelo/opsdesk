e370cb3c9b61c6cbc6b47475eafd6db8b1bd7fab
Marcelo <marcelo.augusto@99x.io>
2025-12-29 13:40:18 -0300

    chore: commit changes

--- apps/api/src/metrics/metrics.controller.ts
+++ apps/api/src/metrics/metrics.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { register } from 'prom-client';
 
 @Controller('metrics')
 export class MetricsController {
   @Get()
-  async metrics() {
-    return register.metrics();
+  async metrics(@Res() res: Response) {
+    res.set('Content-Type', register.contentType);
+    res.send(await register.metrics());
   }
 }
