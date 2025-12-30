d971f2c006764469f0e7feeaaf5054e35dd269ba
Marcelo Augusto <mrclgst10@gmail.com>
2025-12-30 13:56:11 -0300

    test: created e2e tests for tickets and adjusted endpoints
    - initialized the e2e tests for the tickets
    - changed the tickets and users to use /v1 in their endpoints

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -4,22 +4,39 @@ import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('API e2e', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('GET /health should return API health status', async () => {
+    return request(app.getHttpServer()).get('/health').expect(200);
+  });
+
+  it('POST /v1/tickets should create a new ticket', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/v1/tickets')
+      .send({
+        title: 'Teste',
+        description: 'Teste de ticket',
+      })
+      .expect(201);
+
+    expect(res.body.id).toBeDefined();
+  });
+
+  it('GET /v1/tickets should return a list of tickets', () => {
+    return request(app.getHttpServer()).get('/v1/tickets').expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
