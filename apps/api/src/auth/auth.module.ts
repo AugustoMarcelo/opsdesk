ef87fc8ef3f03593c11f38751e9a313e9b37e1d3
Marcelo <marcelo.augusto@99x.io>
2025-12-30 15:27:21 -0300

    feat(auth): implement jwt authentication and guard
    - adjusted the user table to add a new column to password_hash
    - implemented the login controller, service, dto and module
    - implemented the jwt strategy using a global jwt guard
    - created a public decorator to allow accessing the /auth/login without authentication
    - used the nest password framework with jwt strategy

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtStrategy } from './jwt.strategy';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET!,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  controllers: [AuthController],
+  exports: [JwtAuthGuard],
+})
+export class AuthModule {}
--- apps/api/src/db/schema/users.ts
+++ apps/api/src/db/schema/users.ts
@@ -4,5 +4,6 @@ export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
   email: text('email').notNull().unique(),
   name: text('name').notNull(),
+  passwordHash: text('password_hash').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
 });
